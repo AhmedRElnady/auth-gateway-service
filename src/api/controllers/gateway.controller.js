857883dfcd60487f50a2388c369d7f43123194d9
ahmed elnady <dev.elnady@gmail.com>
2019-03-17 05:39:50 +0200

    simple refactor

--- src/api/controllers/gateway.controller.js
+++ src/api/controllers/gateway.controller.js
@@ -12,38 +12,45 @@ router.post('/signup', validate(), async (req, res, next) => {
     try {
         let _apiAdapter,
             GWUserID,
+            GWUserName,
             GWuserRole;
-
+    
         const createdUser = await User.create({
             name: req.body.name,
             email: req.body.email,
             password: await bcrypt.hashPassword(req.body.password),
             role: req.body.role
         });
+        
     
         GWUserID = createdUser.id;
+        GWUserName = createdUser.GWUserName;
         GWuserRole = createdUser.role;
+
+        if (GWuserRole === 'SUPER_ADMIN') {
+            return res.status(201).json({
+                message: `Super Admin Created.`,
+                data: createdUser,
+            });
+        }
+
         const { MSURL, MSPrefix } = await _getMSURLAndPrefix(GWuserRole);  
         _apiAdapter = apiAdapter(MSURL);
         
         // todo: seperate logic in a service file
-        _apiAdapter.post(`/${MSPrefix}/signup`, { GWUserID })
+        _apiAdapter.post(`/${MSPrefix}/signup`, { GWUserID, GWUserName })
             .then(async serviceRes => {
                 console.log("####### response form microService #####", serviceRes.data);
-                
-                const token = await jwt.signToken({ id: GWUserID, role: GWuserRole });
-
                 res.status(201).json({
-                    msg: `user created successfuly in both GATEWAY and ${MSPrefix} services`,  // change msg
+                    message: `user created successfuly in both GATEWAY and ${MSPrefix} services`,  // change msg
                     data: createdUser, // delete password
-                    token
                 });
             });
     } catch (e) {
         next(e);
     }
 });
-
+// TODO: refacotr
 router.post('/login', validate(), async (req, res, next) => {
     try {
         let _apiAdapter,
@@ -51,17 +58,29 @@ router.post('/login', validate(), async (req, res, next) => {
             GWuserRole;
 
         const user = await User.findOne({ email: req.body.email });
-        if (!user) return res.status(404).send("Not registered email.");
-        
-        if(! await bcrypt.isValidPassword(req.body.password, user.password)) return res.status(401).send("Invalid password");
+        if (!user) return res.status(200).json({ success: false, message: 'Incorrect login credentials.' });
+            
 
+        if(! await bcrypt.isValidPassword(req.body.password, user.password)) // return res.status(401).send("Invalid password");
+            return res.status(200).json({ success: false, message: 'Incorrect login credentials.' });
         /*
             i need to store shopId in jwt, don't save admin permissions in jwt for these reasons:
-            so i need to make a request to the service to get user details (shop-admin or customer);
+            i also need to make a request to the service to get user details (shop-admin or customer);
         */
 
        GWUserID = user.id;
        GWuserRole = user.role;
+
+       if(GWuserRole === 'SUPER_ADMIN') { 
+            const token = await jwt.signToken({ id: GWUserID, role: GWuserRole })
+            return res.status(200).json({
+                success: true,
+                msg: 'super admin is logged in, welcome ya kebeer',
+                data: {'userid': user._id, 'name': user.name, 'email': user.email, 'role': user.role},
+                token    
+            })
+        }
+
        const { MSURL, MSPrefix } = await _getMSURLAndPrefix(GWuserRole);
        _apiAdapter = apiAdapter(MSURL);
 
@@ -73,7 +92,9 @@ router.post('/login', validate(), async (req, res, next) => {
                 const token = await jwt.signToken(payload);
                 
                 res.status(200).json({
+                    success: true,
                     msg: `logged in successfuly, welcome`,
+                    data: { 'userid': user._id, 'name': user.name, 'email': user.email, 'role': user.role},
                     token
                 });
             });
@@ -97,14 +118,15 @@ const _getMSURLAndPrefix = (registeredRole) => {
 
 const _setTokenPayload = (GWUserID, GWuserRole, userDetails) => {
     return new Promise((resolve, reject) => {
-        let payload = { // for both super and customers
+        let payload = {
             id: GWUserID,
             role: GWuserRole
         };
 
         if (GWuserRole === "SHOP_ADMIN") {
             payload.approved = userDetails.approved;
-            payload.shopId = userDetails.shopId;
+            payload.activated = userDetails.activated;
+            payload.shopId = userDetails.shopId;    
         }
         resolve(payload);
     });
